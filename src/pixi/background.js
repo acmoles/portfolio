d5b1248df4c5628d00cce8c4d3fbdfab8faff302
Anthony Moles <acmoles@gmail.com>
2018-08-01 09:41:21 +0100

    Reduce blur quality

--- src/pixi/background.js
+++ src/pixi/background.js
@@ -45,7 +45,7 @@ export class Background {
     } else {
       filterSmall.blur = 10;
     }
-    filterSmall.quality = 3;
+    filterSmall.quality = 2;
     filterSmall.autoFit = true;
     this.blurLayerSmall.filters = [filterSmall];
 
@@ -59,7 +59,7 @@ export class Background {
     } else {
       filterLarge.blur = 25;
     }
-    filterLarge.quality = 3;
+    filterLarge.quality = 2;
     filterLarge.autoFit = true;
     this.blurLayerLarge.filters = [filterLarge];
 
