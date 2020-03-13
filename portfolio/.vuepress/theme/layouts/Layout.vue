e4cb4df53027c103b2c80f4b02ced323d95e69ed
Anthony Moles <acmoles@gmail.com>
2020-03-13 10:47:37 +0100

    change noise

--- portfolio/.vuepress/theme/layouts/Layout.vue
+++ portfolio/.vuepress/theme/layouts/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="layout"
+    class="layout noise-heavy"
     :class="pageClasses"
   >
     <Navbar/>
--- portfolio/.vuepress/theme/plugins/plugin-outside-content/enhanceAppFile.js
+++ portfolio/.vuepress/theme/plugins/plugin-outside-content/enhanceAppFile.js
@@ -1,7 +1,7 @@
-import NoiseAdjust from './NoiseAdjust.vue'
+// import NoiseAdjust from './NoiseAdjust.vue'
 import StatusIndicator from './StatusIndicator.vue'
 
 export default ({ Vue }) => {
-  Vue.component('NoiseAdjust', NoiseAdjust)
+  // Vue.component('NoiseAdjust', NoiseAdjust)
   Vue.component('StatusIndicator', StatusIndicator)
 }
