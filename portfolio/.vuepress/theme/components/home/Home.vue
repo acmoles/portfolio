667cad6e1980a2904cc8a94af5d2aeb8a00d8094
Anthony Moles Lyall <anthony.moles.lyall@ecosia.org>
2020-07-07 08:59:58 +0200

    return introquote

--- portfolio/.vuepress/theme/components/home/Home.vue
+++ portfolio/.vuepress/theme/components/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div ref="container" class="container is-fullhd is-home" :style="{ paddingTop: paddingTop }">
+  <div ref="container" class="container is-fullhd is-home">
     <!-- <picture class="test-background">
       <img src="/images/homepage/background-xl.png" alt="">
     </picture> -->
     <!-- <div v-for="n in 5" :class="'test-blur test-blur-' + n"></div> -->
-    <!-- <HomeIntroQuote/> -->
+    <HomeIntroQuote/>
 
     <div class="grid-wrapper" :class="festRowClass">
           <PortfolioItem
@@ -34,14 +34,12 @@ import HomeIntroLarge from '@theme/components/home/HomeIntroLarge.vue'
 import HomeIntroSmall from '@theme/components/home/HomeIntroSmall.vue'
 import HomeIntroQuote from '@theme/components/home/HomeIntroQuote.vue'
 
-import { topPadding } from '@theme/mixins/topPadding.js'
-
-// TODO Manifest.json check, What's requesting logo.png, do favicons,
+// import { topPadding } from '@theme/mixins/topPadding.js'
 
 export default {
-  components: { PortfolioItem, HomeIntroLarge, HomeIntroSmall, HomeIntroQuote },
+  components: { PortfolioItem, HomeIntroQuote },
 
-  mixins: [loadableHero, topPadding],
+  mixins: [loadableHero],
 
   data () {
     return {
@@ -110,8 +108,8 @@ export default {
   @import "@theme/styles/variables.sass"
   @import "@theme/styles/mixins.sass"
 
-  .container.is-home
-    padding-top: 7em
+  // .container.is-home
+  //   padding-top: 7em
 
   // in-view rules for fade staggering
   html:not(.disable-motion)
